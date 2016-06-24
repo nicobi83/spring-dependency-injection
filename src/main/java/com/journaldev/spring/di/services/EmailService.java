8ed4aaae7e7ead534703cd850b57a99e6360b98a
Nicola <bombonati.nicola@gmail.com>
2016-06-24 20:16:21 +0200

    modd

--- src/main/java/com/journaldev/spring/di/services/EmailService.java
+++ src/main/java/com/journaldev/spring/di/services/EmailService.java
@@ -1,13 +1,16 @@
 package com.journaldev.spring.di.services;
 
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by NICOLA on 24/06/2016.
  */
 public class EmailService implements MessageService {
     public boolean sendMessage(String msg, String rec) {
-        Logger logger = new Logger();
+
+        Logger logger = LoggerFactory.getLogger(this.getClass());
         logger.info("E-mail sent to " + rec + "with message " + msg);
         return true;
     }
