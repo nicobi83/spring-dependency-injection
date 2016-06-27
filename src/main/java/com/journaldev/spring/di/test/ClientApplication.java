bd670ea2448190218bb6543fe9fd422415616ee8
Nicola <bombonati.nicola@gmail.com>
2016-06-27 19:41:10 +0200

    modded

---
+++ src/main/java/com/journaldev/spring/di/test/ClientApplication.java
@@ -0,0 +1,20 @@
+package com.journaldev.spring.di.test;
+
+import com.journaldev.spring.di.configuration.DIConfiguration;
+import com.journaldev.spring.di.consumer.MyApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created by NICOLA on 27/06/2016.
+ */
+public class ClientApplication {
+
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext( DIConfiguration.class );
+        MyApplication app = context.getBean( MyApplication.class );
+        app.processMessage( "Ciao, questo è un messaggio di prova", "bombonati.nicola@gmail.com" );
+        context.close();
+    }
+
+}
