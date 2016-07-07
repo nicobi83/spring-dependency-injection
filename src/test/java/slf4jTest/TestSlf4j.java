a3910d42f6089b21ec214c104808691054353434
Nicola <bombonati.nicola@gmail.com>
2016-07-07 19:40:45 +0200

    aggiunta classe per test metodi slf4j

--- src/test/java/slf4jTest/TestSlf4j.java
+++ src/test/java/slf4jTest/TestSlf4j.java
@@ -12,6 +12,7 @@ public class TestSlf4j {
 
     Logger logger = LoggerFactory.getLogger(TestSlf4j.class);
     String s = "prova";
+
     @Test
      public void Hello() {
 
@@ -19,9 +20,25 @@ public class TestSlf4j {
             logger.info("ciao", logger, s);
             logger.trace( "Hello TRACE" );
             logger.error("this is an error");
-            logger.debug("BEDUG!!");
+            logger.debug("DEBUG!!");
             logger.warn("ATTENZIONE!!");
             logger.getName();
      }
+
+    @Test
+    public void Temperature_misuration() {
+
+        Integer t = 30;
+        Integer oldT;
+        Integer temperature = 60;
+
+        oldT = t;
+        t = temperature;
+
+        logger.debug( "Temperature set to {}. Previous temperature was {}", t, oldT );
+        if (temperature.intValue() > 50) {
+            logger.info ( "50 Celsius degree reached!!" );
+        }
+    }
 }
 
