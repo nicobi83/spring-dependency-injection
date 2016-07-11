6b30474a682342d814495ffc114cbf048d55f570
Nicola <bombonati.nicola@gmail.com>
2016-07-11 17:51:08 +0200

    Aggiunta classe TestProperties in test package

--- src/test/java/propertiesTest/TestProperties.java
+++ src/test/java/propertiesTest/TestProperties.java
@@ -1,5 +1,6 @@
 package propertiesTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ public class TestProperties {
     Properties prop = new Properties();
     OutputStream out = null;
     InputStream in = null;
+    InputStream in2 = null;
 
     @Test
     public void writeProperties() {
@@ -73,21 +75,30 @@ public class TestProperties {
     public void loadPropFromClasspath() {
 
         try {
-            String filename = "config.properties";
-            in = TestProperties.class.getClassLoader().getResourceAsStream(filename);
+
+            in = getClass().getClassLoader().getResourceAsStream("application.properties");
+            in2 = getClass().getResourceAsStream("config.properties");
             if (in == null) {
                 logger.error("ERROR!!!! File not present");
                 return;
             }
+            if (in2 == null) {
+                logger.error("ERROR!!!! File not present");
+                return;
+            }
             prop.load(in);
-            logger.info( prop.getProperty("textcolor") );
-            logger.info( prop.getProperty("textfont") );
+            prop.load(in2);
+            logger.info(prop.getProperty("logging.level"));
+            logger.info(prop.getProperty("logging.file"));
+            logger.info(prop.getProperty("textfont"));
+            logger.info(prop.getProperty("textcolor"));
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if(in != null){
+            if (in != null && in !=null) {
                 try {
                     in.close();
+                    in2.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
