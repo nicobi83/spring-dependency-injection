46c983c256ed6821a3b5acba89af433f7df28a44
Nicola <bombonati.nicola@gmail.com>
2016-07-27 11:06:45 +0200

    added test method readXmlFile in TestProperties.Java for reading gnucash database in xml format

---
+++ src/test/java/propertiesTest/ServerCreateList.java
@@ -0,0 +1,22 @@
+package propertiesTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by NICOLA on 21/07/2016.
+ */
+public class ServerCreateList {
+
+    public ServerCreateList(){
+
+    }
+
+
+        ServerModel server = new ServerModel();
+        List <ServerModel> serverList;
+
+
+
+
+}
---
+++ src/test/java/propertiesTest/ServerModel.java
@@ -0,0 +1,16 @@
+package propertiesTest;
+
+/**
+ * Created by NICOLA on 21/07/2016.
+ */
+public class ServerModel {
+
+    int port;
+    int maxThreads;
+    String hostname;
+
+    public ServerModel(){
+
+    }
+
+}
